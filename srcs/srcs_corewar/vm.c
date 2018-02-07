3b72d68debb7a42e7a886775f7494421d4d87b7e
Mathis Pandelé <mapandel@student.42.fr>
2018-02-07 20:25:24 +0100

    Display fully functional

--- srcs/srcs_corewar/vm.c
+++ srcs/srcs_corewar/vm.c
@@ -6,7 +6,7 @@
 /*   By: cfrouin <cfrouin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/15 15:40:32 by cfrouin           #+#    #+#             */
-/*   Updated: 2018/02/07 18:44:52 by cyrillefrouin    ###   ########.fr       */
+/*   Updated: 2018/02/07 20:14:31 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 int				vm_start(t_data *data)
 {
-	display_map(data);
 	while (1)
 	{
 		key_hub(data);
 		if (data->pause != 1)
 		{
-			usleep(100000);
 			prepare_next_op(data);
 			do_next_op(data);
 			data->cycle++;
@@ -30,9 +28,11 @@ int				vm_start(t_data *data)
 				if (data->cycle % data->dump == 0)
 					dump(data);
 			}
-			else
+			else if (!(data->cycle % data->speed))
 				display_map(data);
 		}
+		else if (data->pause)
+			display_map(data);
 		// printf("argsType[0] : %d\nargsType[1] : %d\nargsType[2] : %d\n",
 			// data->champions->argsType[0], data->champions->argsType[1], data->champions->argsType[2]);
 		// printf("args[0] : %d\nargs[1] : %d\nargs[2] : %d\n",
