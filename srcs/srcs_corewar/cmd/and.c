aa52dbc113c41ae51da0261ffb980844314ab86e
Diana WALD <dwald@e1r1p10.42.fr>
2018-02-08 17:55:52 +0100

    cmd add or xor

--- srcs/srcs_corewar/cmd/and.c
+++ srcs/srcs_corewar/cmd/and.c
@@ -6,13 +6,24 @@
 /*   By: cfrouin <cfrouin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 17:00:20 by cfrouin           #+#    #+#             */
-/*   Updated: 2018/01/09 17:28:47 by cfrouin          ###   ########.fr       */
+/*   Updated: 2018/02/08 17:45:15 by dwald            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
+/*
+** Apply an & (bit-to-bit AND) over the first two arguments and store the result
+** in a registry, which is the third argument. Modifies the carry.
+*/
+
 int					corewar_and(t_data *data, t_champion *champ)
 {
+	if (champ->argsType[0] != T_REG || champ->argsType[1] != T_REG
+	|| champ->argsType[2] != T_REG)
+		return (-1);
+	champ->reg[champ->args[2]] = champ->reg[champ->args[0]]
+	& champ->reg[champ->args[1]];
+	champ->carry = (champ->reg[champ->args[2]] == 0) ? 1 : 0;
 	return (1);
 }
