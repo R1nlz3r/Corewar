52c072ef35e64536c6d5ab7d48b08d10d21e2b18
Diana WALD <dwald@e1r1p10.42.fr>
2018-02-08 18:05:03 +0100

    corr or.c

--- srcs/srcs_corewar/cmd/or.c
+++ srcs/srcs_corewar/cmd/or.c
@@ -6,20 +6,18 @@
 /*   By: cfrouin <cfrouin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 16:59:46 by cfrouin           #+#    #+#             */
-/*   Updated: 2018/02/08 17:48:13 by dwald            ###   ########.fr       */
+/*   Updated: 2018/02/08 18:04:33 by dwald            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-int					corewar_or(t_data *data, t_champion *champ)
-{
 /*
 ** Apply an | (bit-to-bit OR) over the first two arguments and store the result
 ** in a registry, which is the third argument. Modifies the carry.
 */
 
-int		corewar_and(t_data *data, t_champion *champ)
+int		corewar_or(t_data *data, t_champion *champ)
 {
 	if (champ->argsType[0] != T_REG || champ->argsType[1] != T_REG
 	|| champ->argsType[2] != T_REG)
