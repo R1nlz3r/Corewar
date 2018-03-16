49119715affdc9a008d7fa9310183792bcfd20ac
Diana WALD <dwald@e2r1p10.42.fr>
2018-03-16 14:12:17 +0100

    correction + new isolated check error

--- srcs/srcs_corewar/cmd/and.c
+++ srcs/srcs_corewar/cmd/and.c
@@ -6,11 +6,32 @@
 /*   By: cfrouin <cfrouin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 17:00:20 by cfrouin           #+#    #+#             */
-/*   Updated: 2018/03/13 17:34:29 by cfrouin          ###   ########.fr       */
+/*   Updated: 2018/03/16 14:05:31 by dwald            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+static	int		check_error_and(int param1, int param2, int param3, int player)
+{
+	bool	error;
+
+	error = false;
+	if ((param1 > 3 || param1 < 1) || (param2 > 3 || param2 < 1) 
+	|| (param3 > 3 || param3 < 1))
+		error = true;
+	else if (param3 != REG_CODE)
+		error = true;
+	if (error == true)
+	{
+		//change to ft_dprintf
+		ft_printf("ERROR: Process %i tries to read instruction's parameter \
+with no valid argument type\n", player);
+		return (-1);
+	}
+	else
+		return (0);
+}
 
 /*
 ** Apply an & (bit-to-bit AND) over the first two arguments and store the result
@@ -19,31 +40,27 @@
 
 int			corewar_and(t_data *data, t_champion *champ)
 {
+	int		parameter[2];
+
 	// Display tests
 	if (data->debug)
 		dump_state("AND", data, champ);
 	//end of tests
-
-	int		parameter[2];
-
 	parameter[0] = champ->argsType[0];
 	parameter[1] = champ->argsType[1];
 	(void)data;
-	if (champ->argsType[2] != REG_CODE)
-	{
-		//change to ft_dprintf
-		ft_printf("ERROR: Process %i tries to read instruction's parameter \
-with no valid argument type\n", champ->number);
+	if (check_error_and(parameter[0], parameter[1], champ->argsType[2],
+	champ->number) == -1)
 		return (-1);
-	}
 	if (parameter[0] == REG_CODE)
 		parameter[0] = champ->reg[champ->args[0]];
 	else if (parameter[0] == DIR_CODE)
 		parameter[0] = champ->args[0];
-/*	else if (parameter[0] == IND_CODE)
+	else if (parameter[0] == IND_CODE)
 	{
-		tmp = find_pc_node(champ, mem_mod(champ->ipc + champ->args[0] % IDX_MOD);
-		parameter[0] = tmp->contentn;
+		ft_printf(RED"Looking for indirect value\n"RESET);
+//		tmp = find_pc_node(champ, mem_mod(champ->ipc + champ->args[0] % IDX_MOD);
+//		parameter[0] = tmp->contentn;
 	}
 	if (parameter[1] == REG_CODE)
 		parameter[1] = champ->reg[champ->args[1]];
@@ -51,12 +68,10 @@ with no valid argument type\n", champ->number);
 		parameter[1] = champ->args[1];
 	else if (parameter[1] == IND_CODE)
 	{
-
+		ft_printf(RED"Looking for indirect value\n"RESET);
 	}
-
-*/
-	champ->reg[champ->args[2]] = champ->reg[champ->args[0]]
-	& champ->reg[champ->args[1]];
+	champ->reg[champ->args[2]] = parameter[0] & parameter[1];
 	champ->carry = (champ->reg[champ->args[2]] == 0) ? 1 : 0;
+//	ft_printf("champ->carry = %d\n",  champ->carry);
 	return (1);
 }
--- srcs/srcs_corewar/cmd/or.c
+++ srcs/srcs_corewar/cmd/or.c
@@ -6,11 +6,32 @@
 /*   By: cfrouin <cfrouin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 16:59:46 by cfrouin           #+#    #+#             */
-/*   Updated: 2018/02/26 15:09:11 by dwald            ###   ########.fr       */
+/*   Updated: 2018/03/16 14:05:11 by dwald            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+static	int		check_error_or(int param1, int param2, int param3, int player)
+{
+	bool	error;
+
+	error = false;
+	if ((param1 > 3 || param1 < 1) || (param2 > 3 || param2 < 1) 
+	|| (param3 > 3 || param3 < 1))
+		error = true;
+	else if (param3 != REG_CODE)
+		error = true;
+	if (error == true)
+	{
+		//change to ft_dprintf
+		ft_printf("ERROR: Process %i tries to read instruction's parameter \
+with no valid argument type\n", player);
+		return (-1);
+	}
+	else
+		return (0);
+}
 
 /*
 ** Apply an | (bit-to-bit OR) over the first two arguments and store the result
@@ -19,12 +40,37 @@
 
 int		corewar_or(t_data *data, t_champion *champ)
 {
+	int		parameter[2];
+
+	// Display tests
+	if (data->debug)
+		dump_state("OR", data, champ);
+	//end of tests
+
+	parameter[0] = champ->argsType[0];
+	parameter[1] = champ->argsType[1];
 	(void)data;
-	if (champ->argsType[0] != REG_CODE || champ->argsType[1] != REG_CODE
-	|| champ->argsType[2] != REG_CODE)
+	if (check_error_or(parameter[0], parameter[1], champ->argsType[2],
+	champ->number) == -1)
 		return (-1);
-	champ->reg[champ->args[2]] = champ->reg[champ->args[0]]
-	| champ->reg[champ->args[1]];
+	if (parameter[0] == REG_CODE)
+		parameter[0] = champ->reg[champ->args[0]];
+	else if (parameter[0] == DIR_CODE)
+		parameter[0] = champ->args[0];
+	else if (parameter[0] == IND_CODE)
+	{
+		ft_printf(RED"Looking for indirect value\n"RESET);
+	}
+	if (parameter[1] == REG_CODE)
+		parameter[1] = champ->reg[champ->args[1]];
+	else if (parameter[1] == DIR_CODE)
+		parameter[1] = champ->args[1];
+	else if (parameter[1] == IND_CODE)
+	{
+		ft_printf(RED"Looking for indirect value\n"RESET);
+	}
+	champ->reg[champ->args[2]] = parameter[0] | parameter[1];
 	champ->carry = (champ->reg[champ->args[2]] == 0) ? 1 : 0;
+//	ft_printf("champ->carry = %d\n",  champ->carry);
 	return (1);
 }
