a705a56ca413c2b45b9e0925bf6de5180c7cda8d
Diana WALD <dwald@e2r3p21.42.fr>
2018-03-19 16:47:32 +0100

    working on verbose and errors

--- srcs/srcs_corewar/cmd/add.c
+++ srcs/srcs_corewar/cmd/add.c
@@ -6,7 +6,7 @@
 /*   By: dwald <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/01 14:57:25 by dwald             #+#    #+#             */
-/*   Updated: 2018/03/19 15:09:21 by dwald            ###   ########.fr       */
+/*   Updated: 2018/03/19 15:58:46 by dwald            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@
 
 int		corewar_add(t_data *data, t_champion *champ) //chekced, should be OK
 {
+	// Display tests
+	if (data->debug)
+		dump_state("ADD", data, champ);
+	//end of tests
 	(void)data;
 	if (champ->argsType[0] != REG_CODE || champ->argsType[1] != REG_CODE
 	|| champ->argsType[2] != REG_CODE)
@@ -29,13 +33,12 @@ int		corewar_add(t_data *data, t_champion *champ) //chekced, should be OK
 with no valid argument type\n", champ->number);
 		return (-1);
 	}
-	champ->reg[champ->args[0]] = champ->reg[champ->args[1]]
-	+ champ->reg[champ->args[2]];
+	champ->reg[champ->args[2]] = champ->reg[champ->args[0]]
+	+ champ->reg[champ->args[1]];
 	champ->carry = (champ->args[0] == 0) ? 1 : 0;
 	if (verbose_operations(data) == 1)
 		ft_printf(CYAN"Player #%i | add r%d + r%d = (%d) -> r%d, carry = %d\n"
-		RESET, champ->number, champ->reg[champ->args[0]],
-		champ->reg[champ->args[1]],	champ->reg[champ->args[2]], champ->args[0],
-		champ->carry);
+		RESET, champ->number, champ->args[0], champ->args[1],
+		champ->reg[champ->args[2]], champ->args[2], champ->carry);
 	return (1);
 }
--- srcs/srcs_corewar/cmd/xor.c
+++ srcs/srcs_corewar/cmd/xor.c
@@ -6,7 +6,7 @@
 /*   By: cfrouin <cfrouin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 16:59:24 by cfrouin           #+#    #+#             */
-/*   Updated: 2018/03/16 16:19:52 by dwald            ###   ########.fr       */
+/*   Updated: 2018/03/19 16:42:08 by dwald            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static	int		check_error_xor(int param1, int param2, int param3, int player)
 	bool	error;
 
 	error = false;
-	if ((param1 > 3 || param1 < 1) || (param2 > 3 || param2 < 1) 
+	if ((param1 > 3 || param1 < 1) || (param2 > 3 || param2 < 1)
 	|| (param3 > 3 || param3 < 1))
 		error = true;
 	else if (param3 != REG_CODE)
@@ -38,18 +38,14 @@ with no valid argument type\n", player);
 ** in a registry, which is the third argument. Modifies the carry.
 */
 
-int		corewar_xor(t_data *data, t_champion *champ)
+int				corewar_xor(t_data *data, t_champion *champ)
 {
 	int		parameter[2];
 
-	// Display tests
-	if (data->debug)
-		dump_state("XOR", data, champ);
-	//end of tests
-
 	parameter[0] = champ->argsType[0];
 	parameter[1] = champ->argsType[1];
-	(void)data;
+	if (data->debug)
+		dump_state("XOR", data, champ);
 	if (check_error_xor(parameter[0], parameter[1], champ->argsType[2],
 	champ->number) == -1)
 		return (-1);
@@ -60,6 +56,8 @@ int		corewar_xor(t_data *data, t_champion *champ)
 	else if (parameter[0] == IND_CODE)
 	{
 		ft_printf(RED"Looking for indirect value\n"RESET);
+//	tmp = find_pc_node(champ, mem_mod(champ->ipc + champ->args[0] % IDX_MOD);
+//	parameter[0] = tmp->contentn;
 	}
 	if (parameter[1] == REG_CODE)
 		parameter[1] = champ->reg[champ->args[1]];
@@ -71,6 +69,9 @@ int		corewar_xor(t_data *data, t_champion *champ)
 	}
 	champ->reg[champ->args[2]] = parameter[0] ^ parameter[1];
 	champ->carry = (champ->reg[champ->args[2]] == 0) ? 1 : 0;
-//	ft_printf("champ->carry = %d\n",  champ->carry);
+	if (verbose_operations(data) == 1)
+		ft_printf(CYAN"Player #%i | xor r%d (%d) ^ r%d (%d) = (%d) -> r%d \
+carry = %i\n"RESET, champ->number, champ->args[0], parameter[0], champ->args[1],
+		parameter[1], champ->reg[champ->args[2]], champ->args[2], champ->carry);
 	return (1);
 }
