0fe1db123f9b2eea8a91498bc7922332103321f5
Diana WALD <dwald@e2r3p21.42.fr>
2018-03-19 15:47:52 +0100

    working on verbose and errors

--- srcs/srcs_corewar/cmd/add.c
+++ srcs/srcs_corewar/cmd/add.c
@@ -6,7 +6,7 @@
 /*   By: dwald <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/01 14:57:25 by dwald             #+#    #+#             */
-/*   Updated: 2018/02/26 15:05:00 by dwald            ###   ########.fr       */
+/*   Updated: 2018/03/19 15:09:21 by dwald            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,24 @@
 ** REG_CODE param[2] = REG_CODE param[0] + REG_CODE param[1]
 */
 
-int		corewar_add(t_data *data, t_champion *champ)
+int		corewar_add(t_data *data, t_champion *champ) //chekced, should be OK
 {
 	(void)data;
 	if (champ->argsType[0] != REG_CODE || champ->argsType[1] != REG_CODE
 	|| champ->argsType[2] != REG_CODE)
+	{
+		//change to ft_dprintf
+		ft_printf("ERROR: Process %i tries to read instruction's parameter \
+with no valid argument type\n", champ->number);
 		return (-1);
+	}
 	champ->reg[champ->args[0]] = champ->reg[champ->args[1]]
 	+ champ->reg[champ->args[2]];
 	champ->carry = (champ->args[0] == 0) ? 1 : 0;
+	if (verbose_operations(data) == 1)
+		ft_printf(CYAN"Player #%i | add r%d + r%d = (%d) -> r%d, carry = %d\n"
+		RESET, champ->number, champ->reg[champ->args[0]],
+		champ->reg[champ->args[1]],	champ->reg[champ->args[2]], champ->args[0],
+		champ->carry);
 	return (1);
 }
