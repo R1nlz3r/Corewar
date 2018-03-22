3f145e9463b1b056592a0c8d720f1c00ad08ca7c
Diana WALD <dwald@e2r1p14.42.fr>
2018-03-22 16:54:16 +0100

    zjump

--- srcs/srcs_corewar/cmd/zjmp.c
+++ srcs/srcs_corewar/cmd/zjmp.c
@@ -6,7 +6,7 @@
 /*   By: dwald <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 11:46:06 by dwald             #+#    #+#             */
-/*   Updated: 2018/03/21 17:04:42 by dwald            ###   ########.fr       */
+/*   Updated: 2018/03/22 16:13:18 by dwald            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 int		corewar_zjmp(t_data *data, t_champion *champ)
 {
 	short	pc_dest;
+	int		index;
 	t_node	*tmp;
 
 	tmp = champ->pc;
+	pc_dest = 0;
+	index = champ->args[0];
 	if (data->debug)
 		dump_state("ZJUMP", data, champ);
 	if (champ->argsType[0] != DIR_CODE)
@@ -29,6 +32,21 @@ with no valid argument type\n", champ->number);
 	}
 	if (champ->carry == 1)
 	{
+	//	ft_printf(CYAN"champ->args[0] = %d\n"RESET,champ->args[0]);
+	//	ft_printf(CYAN"champ->args[0] % IDX_MOD = %d\n"RESET,champ->args[0]%IDX_MOD);
+		pc_dest = mem_mod(champ->oldipc + index);
+		if (verbose_operations(data))
+			ft_printf("Player #%d | zjmp %d OK\n", champ->number, pc_dest);
+	}
+	else
+	{
+		if (verbose_operations(data))
+			ft_printf("Player #%d | zjmp %d FAILED\n", champ->number, pc_dest);
+	}
+	return (1);
+}
+/*
+	{
 		pc_dest = champ->args[0] + champ->ipc;
 		while (champ->ipc < pc_dest--)
 			tmp = tmp->next;
@@ -38,5 +56,4 @@ with no valid argument type\n", champ->number);
 		champ->ipc = champ->args[0] + champ->ipc;
 		printf("champ->ipc = %d", champ->ipc);
 	}
-	return (1);
-}
+*/
